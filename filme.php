e4a5f1fa17bcc3813c0eaac912e0c057b7215d3a
isamary10 <isa.kusuki@gmail.com>
2019-12-09 10:18:19 -0300

    Tratar de filmes não existentes

---
+++ filme.php
@@ -0,0 +1,71 @@
+<?php
+	// Incluindo $filmes
+	include('./includes/filmes.php');
+
+	// Incluindo $generos
+	include('./includes/generos.php');
+
+	$pos = $_GET['pos'];
+	
+	// Verificando se $pos é uma posição inválida do meu array
+	if($pos >= count($filmes)) {
+		include('./erros/404.php');
+		die();
+	}
+
+	// Levantando o filme da posição $pos
+	$filme = $filmes[$pos];
+	
+	// echo('<prev>);
+	// print_r($filme);
+	// echo('</prev');
+	// die();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title><?= $filme['titulo']; ?> - Clube do Filme</title>
+
+	<!-- Meu estilo -->
+	<link rel="stylesheet" href="./assets/css/geral.css">
+	<link rel="stylesheet" href="./assets/css/filme.css">
+</head>
+
+<body>
+
+	<?php include('./includes/nav.php') ?>
+
+	<main>
+
+		<h2><?= $filme['titulo']; ?></h2>
+		<img src="./assets/img/cartazes/cartaz-<?= $pos ?>.png" alt="<?= $filme['titulo']; ?>" class="cartaz">
+		<section>
+
+			<div class="sinopse">
+				<h5>Sinopse</h5>
+				<div><?= $filme['sinopse'] ?></div>
+			</div>
+
+			<div class="censura">
+				<h5>Censura</h5>
+				<div><?= $filme['censura'] === 0 ? 'Livre' : $filme['censura'] . ' anos' ?></div>
+			</div>
+			
+			<div class="critica">
+				<h5>Crítica</h5>
+				<div><?= $filme['critica'] ?></div>
+			</div>
+		</section>
+
+		<?= $filme['trailer'] ?>
+
+		</main>
+</body>
+
+</html>
